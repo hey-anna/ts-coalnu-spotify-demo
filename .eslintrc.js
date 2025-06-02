ff6babf115093011b069f6ab6555ce64fde8a64d
hey-anna <106728222+hey-anna@users.noreply.github.com>
2025-06-02 22:01:03 +0900

    chore: ESLint & Prettier 설정, index 진입점 및 Webpack 스크립트 수정
    - src/index.html 추가 및 root ID를 content로 변경
    - index.tsx에서 ReactDOM 타겟을 content ID로 수정
    - .prettierrc 파일 추가 및 포맷 규칙 설정 (쌍따옴표 등)
    - .eslintrc.js 파일 추가 및 기본 룰 구성, prettier 확장 적용 (충돌 방지용)
    - ESLint 관련 패키지 설치: eslint, @typescript-eslint, react plugin, prettier 설정 포함
    - package.json 내 start/build 스크립트를 CRA → Webpack 기반으로 변경

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier', // 꼭 마지막에 추가
+    ],
+    plugins: ['react', '@typescript-eslint'],
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off', // React 17+에선 필요 없음
+    },
+  };
+  
